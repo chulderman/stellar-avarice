bbf0dea18023a0a81f67b103012db4cc6f0452e2
obscurehero <chase@hulderman.com>
2015-12-09 17:14:35 -0500

    Using basic command prompts; Added version and latest build selection

--- main.py
+++ main.py
@@ -15,11 +15,12 @@ VERSION = "0.0.1"
 # This block parses the commandline with the argparse 		#
 # library. https://docs.python.org/3/library/argparse.html	#
 #############################################################
-parser = argparse.ArgumentParser(prog="stellar_avarice")
+parser = argparse.ArgumentParser(prog="stellar_avarice", description="An Automated Star Citizen Download Utility")
 parser.add_argument('-l', '--latest',
-					help="Display the latest version")
+					help="Display the latest version", action="store_true")
 parser.add_argument('-v', '--version',
-					help="Display program version")
+					help="Display program version", action="version", version='%(prog)s - v{}'.format(VERSION))
+args = parser.parse_args()
 
 #############################################################
 #               ---- latest_version() ----                  #
@@ -27,57 +28,57 @@ parser.add_argument('-v', '--version',
 # Function searches the current directory for *.json files  #
 # It returns the highest number it finds		    		#
 #############################################################
-def latest_version(json_num = 0):
+def latest_build(json_num = 0):
 	dir_contents = [f for f in os.listdir('.') if os.path.isfile(f)]	
+	last_json = json_num
 	for item in dir_contents:
-		last_json = 0
 		if item.split('.')[-1] == 'json':
 			json_num = item.split('.')[0]
 			if last_json < json_num:
 				last_json = json_num
 		else:
 			continue
-	return json_num;
+	return last_json
 
-item_num = int(latest_version())
+#item_num = int(latest_version())
 
-print "Found Latest: " + str(item_num)
+#print "Found Latest: " + str(item_num)
 
 
 
-for i in range (item_num + 1,item_num + BUILD_RANGE + 1):
-	try:
-		manifest_name = str(i) + ".json"
+# for i in range (item_num + 1,item_num + BUILD_RANGE + 1):
+# 	try:
+# 		manifest_name = str(i) + ".json"
 		
-		# Print the latest json that we're trying to test for
-		print 'Trying: {}\r'.format(manifest_name)
-		manifest_response = requests.get(manifest_location + manifest_name)
-		manifest_response.raise_for_status()
+# 		# Print the latest json that we're trying to test for
+# 		print 'Trying: {}\r'.format(manifest_name)
+# 		manifest_response = requests.get(manifest_location + manifest_name)
+# 		manifest_response.raise_for_status()
 		
-		# If it finds the file, doesn't throw an exception and writes the file to disk
-		print "Writing: " + manifest_name
-		manifest_fh = open(manifest_name, "w")
-		manifest_fh.write(manifest_response.read())
-		manifest_fh.close()
-		item_num = manifest_name
-	except Exception as e:
-		if(r.status_code==404):
-			print('%s: Page could not be found.' % e.reason)
-		if(r.status_code>=500):
-			print ('%s: Server error [%s]' % (e.reason,r.status_code))  
+# 		# If it finds the file, doesn't throw an exception and writes the file to disk
+# 		print "Writing: " + manifest_name
+# 		manifest_fh = open(manifest_name, "w")
+# 		manifest_fh.write(manifest_response.read())
+# 		manifest_fh.close()
+# 		item_num = manifest_name
+# 	except Exception as e:
+# 		if(r.status_code==404):
+# 			print('%s: Page could not be found.' % e.reason)
+# 		if(r.status_code>=500):
+# 			print ('%s: Server error [%s]' % (e.reason,r.status_code))  
 
 
-# Open up and parse JSON data
-json_fh = open(str(item_num) + ".json", "r")
-parsed_json = json.load(json_fh)
+# # Open up and parse JSON data
+# json_fh = open(str(item_num) + ".json", "r")
+# parsed_json = json.load(json_fh)
 
-# Setting constants based on JSON data
-base_webseed_url = random.choice(parsed_json["webseed_urls"])
-key_prefix = parsed_json["key_prefix"]
+# # Setting constants based on JSON data
+# base_webseed_url = random.choice(parsed_json["webseed_urls"])
+# key_prefix = parsed_json["key_prefix"]
 
-print "Using: {}".format(item_num)
+# print "Using: {}".format(item_num)
 
-	
+
 #for file_name in parsed_json["file_list"]:
 #	list = []
 #	file_url = base_webseed_url + "/" + key_prefix + "/" + file_name
@@ -108,3 +109,10 @@ print "Using: {}".format(item_num)
 #	    else:
 #	    	for block in response.iter_content(1024):
 #	        fh.write(block)
+def main():
+	if args.latest:
+		print "Latest Build: {}".format(latest_build())
+		sys.exit(0)
+	else:
+		sys.exit(0)
+main()
