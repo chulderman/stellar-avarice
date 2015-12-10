4027e6728967fea87bd5b1bf73eede9e54bb2869
obscurehero <chase@hulderman.com>
2015-12-09 19:42:27 -0500

    Implemented Downloading using stream

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ def new_build_check():
 			if(manifest_response.status_code==404):
 				print('%s: Page could not be found.' % e.reason)
 			if(manifest_response.status_code>=500):
-				print ('%s: Server error [%s]' % (e.reason, manifest_response.status_code))  
+				print ('%s: Server error [%s]' % (e.reason, manifest_response.status_code))
 
 #############################################################
 #               ---- version_compare() ----                 #
@@ -96,48 +96,53 @@ def version_compare(num_check):
 		latest_num=num_check
 	return latest_num
 
-
 # # Open up and parse JSON data
-# json_fh = open(str(item_num) + ".json", "r")
-# parsed_json = json.load(json_fh)
+def parse_json():
 
-# # Setting constants based on JSON data
-# base_webseed_url = random.choice(parsed_json["webseed_urls"])
-# key_prefix = parsed_json["key_prefix"]
+	latest_json = str(latest_build())
+	print "Using: {}".format(latest_json)
+	json_fh = open(latest_json + ".json", "r")
+	parsed_json = json.load(json_fh)
+	return parsed_json
+	
 
-# print "Using: {}".format(item_num)
+def download_build():
+	parsed_json = parse_json()
 
+	base_webseed_url = random.choice(parsed_json["webseed_urls"])
+	key_prefix = parsed_json["key_prefix"]
 
-#for file_name in parsed_json["file_list"]:
-#	list = []
-#	file_url = base_webseed_url + "/" + key_prefix + "/" + file_name
-#	if "/" in file_name:
-#		for item in file_name.split('/'):
-#			list.append(item)
-#		file_name = list.pop(-1)
-#		file_path = os.path.join(*list)
-#		if not os.path.isdir(file_path):
-#			try:
-#				os.makedirs(file_path)
-#			except OSError:
-#				print "Error creating directory: " + file_path
-#				
-#	file = os.path.join(file_path, file_name)
-#	# Begin Downloading Part
-#	
-#	print("Downloading: %s" % file)
-#	
-#	response = requests.get(file_url)
-#	
-#	if os.path.isfile(file):
-#		print("File Exists - Updating")
-#	
-#	with open(file, "w") as fh:
-#	    if not response.ok:
-#	        print('File download was unsuccessful')
-#	    else:
-#	    	for block in response.iter_content(1024):
-#	        fh.write(block)
+	# Setting constants based on JSON data
+	for file_name in parsed_json["file_list"]:
+		list = []
+		file_url = base_webseed_url + "/" + key_prefix + "/" + file_name
+		if "/" in file_name:
+			for item in file_name.split('/'):
+				list.append(item)
+			file_name = list.pop(-1)
+			file_path = os.path.join(*list)
+			if not os.path.isdir(file_path):
+				try:
+					os.makedirs(file_path)
+				except OSError:
+					print "Error creating directory: " + file_path
+					
+		file = os.path.join(file_path, file_name)
+		# Begin Downloading Part
+		
+		print("Downloading: %s" % file)
+		
+		response = requests.get(file_url, stream=True)
+		
+		if os.path.isfile(file):
+			print("File Exists - Updating")
+		
+		with open(file, "w") as fh:
+		    if not response.ok:
+		        print('File download was unsuccessful')
+		    else:
+		    	for block in response.iter_content(1024):
+		        	fh.write(block)
 def main():
 	if args.latest:
 		print "Latest Build: {}".format(latest_build())
@@ -145,5 +150,6 @@ def main():
 	else:
 		## This isn't working right now
 		new_build_check()
+		download_build()
 		sys.exit(0)
 main()
